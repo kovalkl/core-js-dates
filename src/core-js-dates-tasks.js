c8d24157e2c543d33c3f73ac6a582b29f19e2a3c
Koval Anastasiia <ilovepokyshat@gmail.com>
2024-02-29 23:27:14 +0300

    feat: add getNextFriday func

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -70,8 +70,11 @@ function getDayName(date) {
  * Date('2024-02-13T00:00:00Z') => Date('2024-02-16T00:00:00Z')
  * Date('2024-02-16T00:00:00Z') => Date('2024-02-23T00:00:00Z')
  */
-function getNextFriday(/* date */) {
-  throw new Error('Not implemented');
+function getNextFriday(date) {
+  const daysToFriday =
+    date.getDay() < 5 ? 5 - date.getDay() : 12 - date.getDay();
+
+  return new Date(date.getTime() + daysToFriday * 24 * 60 * 60 * 1000);
 }
 
 /**
